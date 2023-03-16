8a43a6ff2e570d0b84dad2cc79b365ac9e5c2a50
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-03-16 17:25:13 +0200

    asd

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,25 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="yandex-verification" content="9eb0983f5a804ccc" />
         </Head>
+        <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+         
+            ym(92837588, "init", {
+                 clickmap:true,
+                 trackLinks:true,
+                 accurateTrackBounce:true,
+                 webvisor:true
+            });
+      `,
+          }}
+        />
         <body>
           <Main />
           <NextScript />
