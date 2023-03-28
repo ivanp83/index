92041636ebb2682bfb4641d66516e2decf4e4adb
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-03-28 15:26:57 +0200

    asd

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,10 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link href="/fonts/style.css" rel="stylesheet" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+          />
           <meta name="yandex-verification" content="9eb0983f5a804ccc" />
           <link
             rel="apple-touch-icon"
